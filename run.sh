3b79086eb92021936b6815a63f3f7b4e80f55476
Leendert Meyer <iamlennym@gmail.com>
2021-12-20 22:01:04 +0200

    Removed the bogus cp command

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #docker run -e PROVISION_KEY=`cat provision_key` -d --rm --privileged local_zscaler
-cp provision_key opt/zscaler/var
+#cp provision_key opt/zscaler/var
 container_id=`docker run -d --rm --privileged local_zscaler_zpa`
 docker cp provision_key $container_id:/opt/zscaler/var
 docker exec -ti $container_id tail -f /var/log/messages
